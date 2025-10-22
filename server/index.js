b7a68a35cdba68b0f31a396ac9be176e45d0070b
Colman McGuan <csmcguan@gmail.com>
2025-10-22 19:47:11 -0400

    add initial project files

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const cors = require("cors");
+
+app.use(cors());
+
+app.listen(port, () => {
+    console.log("server listening on port " + port);
+});
+
+app.get("/", (req, res) => {
+    res.send("hello!!!!!!!!!!!!");
+})
