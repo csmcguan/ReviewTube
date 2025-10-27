02e4f6e244e192a193ac79da9f70954374065da6
Colman McGuan <csmcguan@gmail.com>
2025-10-26 20:43:17 -0400

    finalize prod build settings

--- server/index.js
+++ server/index.js
@@ -1,17 +1,14 @@
-import express from "express";
-import cors from "cors";
-import path from "path";
-import morgan from "morgan"
-import {fileURLToPath} from "url";
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(express.urlencoded({extended: true}));
 
+// run dev
 if (process.env.NODE_ENV == "development") {
   app.use(cors({ origin: 'http://localhost:3000' }));
 }
@@ -26,17 +23,24 @@ app.post("/api/login", (req, res) => {
 
   return res.status(401).json({ ok: false, error: "Invalid credentials" });
 });
-app.get("/api/ping", (req, res) => {
+
+app.get("/api/ping", (_req, res) => {
   res.json({ ok: true, msg: "pong" });
-});//Testing if server is listening to client requests
-app.listen(PORT, () => {
-    console.log(`server listening on port ${PORT}`);
 });
 
 // serve for prod
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (rq, res) =>
-    res.sendFile(path.join(__dirname, "../client/build/index.html"))
-  );
+  const buildPath = path.join(__dirname, "../client/build");
+  app.use(express.static(buildPath));
+  app.use((req, res, next) => {
+    if (req.method !== "GET") {
+      return (next());
+    }
+    res.sendFile(path.join(buildPath, "index.html"));
+  });
 }
+
+//Testing if server is listening to client requests
+app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`);
+});
