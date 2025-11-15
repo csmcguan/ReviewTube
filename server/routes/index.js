7f2f2bee4720eb38c1fbe65d22776eff68a26d3e
Colman McGuan <csmcguan@gmail.com>
2025-11-15 17:07:22 -0500

    added user handling

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,33 @@
 import express from "express";
+import ping from "./ping.routes.js";
 import searchRoutes from "./search.routes.js";
 import usersRoutes from "./users.routes.js";
 import reviewRoutes from "./review.routes.js";
 
 const router = express.Router();
-router.use("/search", searchRoutes);
-router.use("/users", usersRoutes);
-router.use("/reviews", reviewRoutes);
+
+// ping route
+router.use(
+    "/ping", 
+    ping
+);
+
+// search route
+router.use(
+    "/search", 
+    searchRoutes
+);
+
+// user routes
+router.use(
+    "/users", 
+    usersRoutes
+);
+
+// review routes
+router.use(
+    "/reviews", 
+    reviewRoutes
+);
 
 export default router;
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,5 +1,37 @@
 import express from "express";
+import { userController } from "../controllers/users.controller.js";
 
 const router = express.Router();
+
+// signup for ReviewTube!
+router.post("/signup", userController.signup);
+
+// login enpoint
+router.post("/login", userController.login);
+
+// logout endpoint
+router.post("/logout", userController.logout);
+
+// follow another user
+router.post(
+    "/:userId/follow/:targetId",
+    userController.follow
+);
+
+// unfollow another user
+router.post(
+    "/:userId/unfollow/:targetId",
+    userController.blockUser
+);
+
+// block another user
+router.post(
+    "/:userId/block/:targetId",
+    userController.blockUser
+);
+
+// Profiles
+router.get("/me", userController.getMyProfile);         // current logged-in user
+router.get("/:userId", userController.getUserProfile);  // view another user's profile
 
 export default router;
